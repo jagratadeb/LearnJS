b3e633f76aff27edd4f436c2b20cba7b0d1cf30c
JDdevelopp <jagratadeb2004@gmail.com>
2024-12-02 21:13:44 +0530

    Update 2forIn.js

--- 6Loops/2forIn.js
+++ 6Loops/2forIn.js
@@ -1,7 +1,9 @@
-let numbers = [1, 2, 3, 4, 5];
-
 // For-in loop (for object properties)
-let obj = { a: 1, b: 2, c: 3 };
+let obj = { 
+  a: 1, 
+  b: 2, 
+  c: 3 
+};
 console.log("For-in loop (object properties):");
 for (let key in obj) {
   console.log(key + ": " + obj[key]);
