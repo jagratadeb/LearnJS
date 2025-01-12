59fe022b5f45f9d21185b0f1bbb0d31b0e131475
JDdevelopp <jagratadeb2004@gmail.com>
2025-01-12 13:43:19 +0530

    Arrays Update

--- 13Arrays/14Find.js
+++ 13Arrays/14Find.js
@@ -3,5 +3,8 @@ let arr = [1, 2, 3, 4, 5];
 // Find - finding the first element that satisfies a test
 // The `find` method returns the first element in the array that satisfies the provided testing function.
 // In this case, the function checks if the element is greater than 3.
-let foundElement = arr.find(x => x > 3);
-console.log("Found Element:", foundElement); // 4
+let foundElement = arr.find(
+    (x) => x>3
+);
+
+console.log(foundElement);
--- 13Arrays/9Splice.js
+++ 13Arrays/9Splice.js
@@ -3,4 +3,4 @@ let arr1 = [1, 2, 3, 4, 5];
 // Splice - adding/removing elements from an array
 // Syntax - startPosition,deleteCount, insert item1, insert item2.....
 arr1.splice(2, 1, 'a', 'b');
-console.log("Array after splice:", arr1); // [0, 2, 'a', 'b', 4, 5]
+console.log("Array after splice:", arr1); // [1, 2, 'a', 'b', 4, 5]
