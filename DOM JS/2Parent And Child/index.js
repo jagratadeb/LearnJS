0fcd29aa0414d0e2272f4c80c16d837d25992baa
JDdevelopp <jagratadeb2004@gmail.com>
2025-01-20 00:00:10 +0530

    Selectors

---
+++ DOM JS/2Parent And Child/index.js
@@ -0,0 +1,12 @@
+// Parent 
+let child = document.querySelector('li');
+let parent = child.parentElement;
+console.log(parent);
+
+// Children
+let parent1 = document.querySelector('ul');
+// let child1 = parent1.children;
+console.log(parent1.children[0]);
+console.log(parent1.children[1]);
+console.log(parent1.children[2]);
+console.log(parent1.children[3]);
---
+++ DOM JS/3Sibling/index.js
@@ -0,0 +1,8 @@
+// Previous
+let element = document.querySelector('.hero');
+if(element.previousElementSibling == null){
+    console.log('No Previous Sibling');
+}
+else{
+    console.log(element.previousElementSibling);
+}
