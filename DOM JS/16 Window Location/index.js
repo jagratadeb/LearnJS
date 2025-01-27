7a595646b92e38c2e0f2250dc485ee2bd21620d1
JDdevelopp <jagratadeb2004@gmail.com>
2025-01-27 23:40:53 +0530

    Update

---
+++ DOM JS/16 Window Location/index.js
@@ -0,0 +1,5 @@
+// Get the element with the ID 'loc'
+let loc = document.getElementById('loc');
+
+// Set the inner HTML of the element to the current URL of the webpage
+loc.innerHTML = window.location;
