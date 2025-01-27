d40928de5121638936cb1cd8f1c850e2d4dd415d
JDdevelopp <jagratadeb2004@gmail.com>
2025-01-27 23:50:07 +0530

    Pop Ups

---
+++ DOM JS/17 Pop ups/index.js
@@ -0,0 +1,8 @@
+// Display an alert box with a message
+alert("This is an alert!");
+
+// Display a prompt box with a message and a default input value
+prompt("This is a prompt:", 'Yes it is');
+
+// Display a confirm box with a message
+confirm("This is a 'confirm' pop-up");
