527c5c0758a6b2dd494dc0abf203a86ee5037633
JDdevelopp <jagratadeb2004@gmail.com>
2025-02-02 00:43:01 +0530

    JavaScript

---
+++ 004 Interview Practice/005 String Reverse/index.js
@@ -0,0 +1,9 @@
+let str = "Hello World!";
+console.log(str);
+
+function revString(striiing) {
+    return striiing.split('').reverse().join(''); 
+}
+
+
+console.log(revString(str));
---
+++ 004 Interview Practice/006 Even Or Odd/index.js
@@ -0,0 +1,11 @@
+let num = 6;
+
+const check = (n) => {
+    if (n % 2 == 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+
+console.log(check(num))
