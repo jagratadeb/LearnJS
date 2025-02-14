a83f840ea921495d61f5d1504572bb5b5b6c5641
Jagrata Deb <jagratadeb2004@gmail.com>
2025-02-15 00:32:13 +0530

    commit

--- 003 Mini Projects/009 Countdown App/index.js
+++ 003 Mini Projects/009 Countdown App/index.js
@@ -1,18 +1,19 @@
-const targetDate = new Date('2026-01-01 00:00:00');
+const targetDate = new Date('2023-07-19 00:00:00');
 
-function updateCount() {
+function updateDaysPassed() {
     const currentDate = new Date();
-    const timeDifference = targetDate - currentDate;
+    const timeDifference = currentDate - targetDate;
 
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365));
+    const days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    document.getElementById('countdown').innerText = 
-        `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
+    document.getElementById('daysPassed').innerText = 
+        `${years} Years ${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
 
-    setTimeout(updateCount, 1000);
+    setTimeout(updateDaysPassed, 1000); // Update every second
 }
 
-updateCount();
+updateDaysPassed();
