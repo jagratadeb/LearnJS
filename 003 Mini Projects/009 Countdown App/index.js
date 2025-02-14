8dcf2a982b85dd1d21ebd1e0ebd548040b2af007
Jagrata Deb <jagratadeb2004@gmail.com>
2025-02-14 23:11:10 +0530

    Update CSS

--- 003 Mini Projects/009 Countdown App/index.js
+++ 003 Mini Projects/009 Countdown App/index.js
@@ -2,18 +2,15 @@ const targetDate = new Date('2026-01-01 00:00:00');
 
 function updateCount() {
     const currentDate = new Date();
-
     const timeDifference = targetDate - currentDate;
 
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    document.getElementById('countdown').innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    document.getElementById('countdown').innerText = 
+        `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
 
     setTimeout(updateCount, 1000);
 }
