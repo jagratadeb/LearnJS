47cb3104f09ce8017e3c4c7b2372d0115d119cb1
Jagrata Deb <jagratadeb2004@gmail.com>
2025-02-20 23:35:37 +0530

    Add more interview questions

---
+++ 004 Interview Practice/009 Car Object/index.js
@@ -0,0 +1,14 @@
+let car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2015,
+}
+
+car.startEngine = function() {
+  console.log("Engine Started");
+}
+
+console.log(car);
+
+
+car.startEngine(); // Engine Started
