662e3933896c49ba81bb5b908a37619a2893559b
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-26 22:01:43 +0530

    Improve code for Short Circuiting

--- 001 Basic Concepts/004 Operators/08ShortCircuiting.js
+++ 001 Basic Concepts/004 Operators/08ShortCircuiting.js
@@ -17,8 +17,12 @@ console.log(result4);
 
 
 // Nullish Coalescing
+// returns the right hand side value only if the left side is null or undefined.
 let result5 = null ?? "Default";
 console.log(result5);
 
-let result6 = 0 ?? "JD";
+let result6 = undefined ?? "Right Hand Value";
 console.log(result6);
+
+let result7 = 0 ?? "JD";
+console.log(result7);
