06e4cc46f2da4b4076610fe3834896746bc7eb73
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-31 19:10:06 +0530

    Add more escape sequences

--- 001 Basic Concepts/009 EscapeSequences/1escapeSequence.js
+++ 001 Basic Concepts/009 EscapeSequences/1escapeSequence.js
@@ -25,3 +25,33 @@ console.log("Hello\"World!");
 // Unicode (\uXXXX) - include Unicode character
 console.log("Hello\u0020World!");
 // Output: Hello World!
+
+// Carriage Return (\r) - moves cursor to the beginning of the line
+console.log("Hello\rWorld!");
+// Output: World!
+
+// Backspace (\b) - removes the previous character
+console.log("Hello\bWorld!");
+// Output: HellWorld!
+
+// Form Feed (\f) - advances the paper feed (not visible in most consoles)
+console.log("Hello\fWorld!");
+// Output: Hello\fWorld!
+
+// Vertical Tab (\v) - vertical tab (not visible in most consoles)
+console.log("Hello\vWorld!");
+// Output: Hello\vWorld!
+
+// Null Character (\0) - null character
+console.log("Hello\0World!");
+// Output: Hello\0World!
+
+// Hexadecimal (\xXX) - character with the given hex code
+console.log("Hello\x57orld!");
+// Output: HelloWorld!
+
+// Template literal with escape sequences
+console.log(`Line1\nLine2\tTabbed\"Quote\"`);
+// Output:
+// Line1
+// Line2	Tabbed"Quote"
