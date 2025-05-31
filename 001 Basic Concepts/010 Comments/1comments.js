fc4c42a0e7c3d943b0497a248d1ab8677619344f
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-31 20:04:03 +0530

    meaningful comment

--- 001 Basic Concepts/010 Comments/1comments.js
+++ 001 Basic Concepts/010 Comments/1comments.js
@@ -1,10 +1,9 @@
 // Single-line comment
 // This is a single-line comment
-console.log("This is a single-line comment example.");
+
 
 // Multi-line comment
 /* 
    This is a multi-line comment
    It can span multiple lines
 */
-console.log("This is a multi-line comment example.");
