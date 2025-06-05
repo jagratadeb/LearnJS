961b7a71652597142ed46a5fe243a2807bc53426
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-06 00:14:26 +0530

    Format improvement

--- 002 DOM JS/0004 Append/index.js
+++ 002 DOM JS/0004 Append/index.js
@@ -2,15 +2,15 @@
 const ul = document.querySelector('ul');
 
 // Create a new <li> element
-const NewEle1 = document.createElement('li');
+const newEle1 = document.createElement('li');
 // Set the text content of the new <li> to 'Hello'
-NewEle1.textContent = 'Hello';
+newEle1.textContent = 'Hello';
 // Append the new <li> element to the <ul> element
-ul.appendChild(NewEle1);
+ul.appendChild(newEle1);
 
 // Create another new <li> element
-const NewEle2 = document.createElement('li');
+const newEle2 = document.createElement('li');
 // Set the text content of the new <li> to 'World'
-NewEle2.textContent = 'World';
+newEle2.textContent = 'World';
 // Append the new <li> element to the <ul> element
-ul.appendChild(NewEle2);
+ul.appendChild(newEle2);
