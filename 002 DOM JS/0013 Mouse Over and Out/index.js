035fddceaa2f886388905ab7cd24e8f5253e4bbb
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-06 16:42:37 +0530

    Improve the code and reduce redundancy

--- 002 DOM JS/0013 Mouse Over and Out/index.js
+++ 002 DOM JS/0013 Mouse Over and Out/index.js
@@ -1,9 +1,9 @@
+let ele = document.getElementById("container");
+
 function onMouseOver() {
-  let ele = document.getElementById("container");
-    ele.style.backgroundColor = "#1b1b1b";
+  ele.style.backgroundColor = "#1b1b1b";
 }
 
-function onMouseOut() {
-  let ele = document.getElementById("container");
-    ele.style.backgroundColor = "white";
+let onMouseOut = () => {
+  ele.style.backgroundColor = "white";
 }
